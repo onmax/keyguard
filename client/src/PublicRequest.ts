0d5d6a9fe8c126a233312913959f544c6eaa6ebf
Sören <hello@soerenschwert.de>
2021-10-05 14:44:53 +0200

    Add support for unstaking transactions

--- client/src/PublicRequest.ts
+++ client/src/PublicRequest.ts
@@ -31,7 +31,7 @@ export type TransactionInfo = {
     keyPath: string,
     senderLabel?: string,
     sender: Uint8Array,
-    senderType: Nimiq.Account.Type,
+    senderType: Nimiq.Account.Type | 3,
     recipient: Uint8Array,
     recipientType?: Nimiq.Account.Type,
     value: number,
